75ed81213385ef4923875b9e109fbfcdd47edd1e
SHAFAYET TUHIN <85475193+Shafayet-tuhin@users.noreply.github.com>
2021-09-17 01:28:40 +0600

    Create D - Fixed Password.cpp

---
+++ D - Fixed Password.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+int a,b;
+    a=1999;
+    while(1){
+        cin>>b;
+        if(b==a){
+            printf("Correct\n");
+            break;
+        }
+        else{
+            printf("Wrong\n");
+        }
+    }
+    return 0;
+}
+
